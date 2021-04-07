0999c251f68b30b3c86a1c35fc2adbcbac4183ba
Martin Rudzis <mrudzis@uw.edu>
2021-04-07 16:31:43 -0700

    Updated csv functionality

--- src/com/uw/css/relationshipsDownloader/relationshipsDownloader.java
+++ src/com/uw/css/relationshipsDownloader/relationshipsDownloader.java
@@ -4,8 +4,15 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Scanner;
 
 public class relationshipsDownloader {
     private static class Node {
@@ -29,23 +36,30 @@ public class relationshipsDownloader {
         try {
             String url = "https://cwe.mitre.org/data/definitions/" + ID + ".html";
             Document doc = Jsoup.connect(url).get();
-            Elements rows = doc.select("div#relevant_table").get(0).select("tr");
-            ArrayList<Element> relatives = new ArrayList<>();
+            if (doc.select("div#relevant_table").size() > 0) {
+                Elements rows = doc.select("div#relevant_table").get(0).select("tr");
+                ArrayList<Element> relatives = new ArrayList<>();
 
-            for (Element row : rows) {
-                if (row.child(0).text().equals("ChildOf")) {
-                    relatives.add(row);
+                for (Element row : rows) {
+                    if (row.child(0).text().equals("ChildOf")) {
+                        relatives.add(row);
+                    }
+                }
+                if (relatives.size() > 0) {
+                    for (Element row : relatives) {
+                        getRelationships(n, row.child(2).text());
+                    }
+                } else if (!n.getID().equals(ID)) {
+                    if (!n.getParents().contains(ID)) {
+                        n.setParents(ID);
+                    }
+                } else {
+                    n.setParents("0");
                 }
             }
-            if (relatives.size() > 0) {
-                for (Element row : relatives) {
-                    getRelationships(n, row.child(2).text());
-                }
+            else {
+                n.setParents("0");
             }
-            else if (!n.getID().equals(ID)) {
-                n.setParents(ID);
-            }
-            else { n.setParents("0"); }
         }
         catch (IOException e) {
             e.printStackTrace();
@@ -53,12 +67,50 @@ public class relationshipsDownloader {
         return n;
     }
 
-    public static void main(String[] args) {
-        String ID = "707";
-        Node n = new Node();
-        n.setID(ID);
-        getRelationships(n, ID);
+    public static HashMap<String, ArrayList<String>> categorizeData(Node n) {
+        HashMap<String, ArrayList<String>> data = new HashMap<>();
+        data.put("ID", new ArrayList<>(Arrays.asList(n.getID())));
+        data.put("Ancestors", n.getParents());
+        return data;
+    }
 
-        System.out.println(n.getParents());
+    public static void writeToCSV(ArrayList<HashMap<String, ArrayList<String>>> data) throws FileNotFoundException {
+        PrintWriter p = new PrintWriter("src\\com\\uw\\css\\relationshipsDownloader\\CWE_DATA");
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("ID, Parent");
+        sb.append("\n");
+
+        for (HashMap<String, ArrayList<String>> h : data) {
+            String ID = h.get("ID").get(0);
+            String parentList = h.get("Ancestors").toString();
+            String parents = parentList.substring(1, parentList.length() - 1);
+            sb.append(ID);
+            sb.append(',');
+            sb.append(parents);
+            sb.append('\n');
+        }
+        p.write(sb.toString());
+        p.flush();
+        p.close();
+    }
+
+    public static void main(String[] args) throws IOException {
+        ArrayList<HashMap<String, ArrayList<String>>> relationshipData = new ArrayList<>();
+        File IDs = new File("src\\com\\uw\\css\\relationshipsDownloader\\CWE_IDs");
+        Scanner idReader = new Scanner(IDs);
+
+        while (idReader.hasNextLine()) {
+            String ID = idReader.nextLine();
+            System.out.println(ID);
+            Node n = new Node();
+            n.setID(ID);
+            Node data = getRelationships(n, ID);
+            relationshipData.add(categorizeData(data));
+        }
+
+        writeToCSV(relationshipData);
+
+        idReader.close();
     }
 }
